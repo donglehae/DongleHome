97df0e056868b3880ac78d3a05807326fce06c98
Dongledh <dongledh@gmail.com>
2020-07-05 18:53:39 +0900

    modify

--- src/main/java/com/intflow/stock/controller/MainController.java
+++ src/main/java/com/intflow/stock/controller/MainController.java
@@ -21,12 +21,8 @@ public class MainController {
         List<Map<String, Object>> datasKosdaq = stockService.selectKosdaq();
         List<Map<String, Object>> datasFut = stockService.selectFut();
 
-        System.out.println("시간 : " + datasKospi.get(0).get("time") + " || " + "시세 : " + datasKospi.get(0).get("quote"));
-        System.out.println(datasKosdaq.get(0));
-        System.out.println(datasFut.get(0));
-
         for(int i = 0; i < datasKospi.size(); i++) {
-            model.addAttribute("label_" + (i+1), datasKospi.get(datasKospi.size()-1-i).get("time"));
+            model.addAttribute("label_" + (i+1), datasKospi.get(i).get("time"));
             model.addAttribute("kospi_" + (i+1), Double.parseDouble(datasKospi.get(i).get("quote").toString().replaceAll(",", "")));
             model.addAttribute("kosdaq_" + (i+1), Double.parseDouble(datasKosdaq.get(i).get("quote").toString().replaceAll(",", "")));
             model.addAttribute("fut_" + (i+1), Double.parseDouble(datasFut.get(i).get("quote").toString().replaceAll(",", "")));
